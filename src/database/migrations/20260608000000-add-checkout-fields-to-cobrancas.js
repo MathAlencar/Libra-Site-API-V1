389bb8129bf01f0493e740b0e76e2863d542999b
luana.araujo <luana.barreto@socin.com.br>
2026-06-07 22:21:17 -0300

    fix: migration update

---
+++ src/database/migrations/20260608000000-add-checkout-fields-to-cobrancas.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const table = await queryInterface.describeTable('Cobrancas');
+
+    if (!table.payment_link_id) {
+      await queryInterface.addColumn('Cobrancas', 'payment_link_id', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      });
+    }
+
+    if (!table.checkout_url) {
+      await queryInterface.addColumn('Cobrancas', 'checkout_url', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      });
+    }
+  },
+
+  async down(queryInterface) {
+    const table = await queryInterface.describeTable('Cobrancas');
+
+    if (table.checkout_url) {
+      await queryInterface.removeColumn('Cobrancas', 'checkout_url');
+    }
+
+    if (table.payment_link_id) {
+      await queryInterface.removeColumn('Cobrancas', 'payment_link_id');
+    }
+  },
+};
